0cde0a52ec734c80221b48f23e4915b8de37f3a9
SalimLázaro <salimlazaro04@gmail.com>
2026-06-28 23:39:43 -0300

    corrigir werkzeug producao

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ def not_found(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
